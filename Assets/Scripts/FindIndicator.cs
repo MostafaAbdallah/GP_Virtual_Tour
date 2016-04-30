68e762c5bc8ce83de4abaa09c52bb38796f9fddb
MostafaAbdallah <mostafaabdallah42@gmail.com>
2016-04-30 12:27:05 -0700

    Add rayCasting from the center

---
+++ Assets/Scripts/FindIndicator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindIndicator : MonoBehaviour {
+
+    public Camera mainCamera;
+    RaycastHit hit;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            print("I'm looking at " + hit.transform.name);
+        else
+            print("I'm looking at nothing!");
+	}
+}
