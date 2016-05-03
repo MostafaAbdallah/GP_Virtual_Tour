a413c81261f83ca16bf81abd67afafddb371a798
MostafaAbdallah <mostafaabdallah42@gmail.com>
2016-05-03 08:18:02 -0700

    Add panorama shader

--- Assets/Scripts/ChangePlaneImage.cs
+++ Assets/Scripts/ChangePlaneImage.cs
@@ -16,15 +16,44 @@ public class ChangePlaneImage : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.C)) {
-			changeTexture ();
+            changeTextureFromResources();
 		}
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+           StartCoroutine( changeTextureFromServer());
+        }
 	}
 
-	void changeTexture(){
+    void changeTextureFromResources()
+    {
 		Texture _texture;
 
 		_texture = Resources.Load(imageName) as Texture;
 		P.GetComponent<Renderer>().material.mainTexture = _texture;
 
 	}
+
+    IEnumerator changeTextureFromServer()
+    {
+        Texture2D _texture = new Texture2D(4, 4, TextureFormat.DXT1, false);
+
+        //_texture = Resources.Load(imageName) as Texture;
+        
+        print("Load image in = " + imageName);
+        
+        while (true)
+        {
+            // Start a download of the given URL
+            var www = new WWW(imageName);
+
+            // wait until the download is done
+            yield return www;
+
+            // assign the downloaded image to the main texture of the object
+            www.LoadImageIntoTexture(_texture);
+            P.GetComponent<Renderer>().material.mainTexture = _texture;
+            break;
+        }
+
+    }
 }
